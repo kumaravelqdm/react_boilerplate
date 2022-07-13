55f9a6f1e1ae943d941d03ae5bef16f6605218f5
kumaravel <kumaravel@crayond.com>
2022-07-14 01:57:15 +0530

    multi lingual installed

--- src/App.auth.js
+++ src/App.auth.js
@@ -6,39 +6,45 @@
  * @desc Providing the AuthContext from /src/context which is used in the /src/App.js.
  */
 
-import React from "react";
-import { BorderLinearProgress } from "qdm-component-library";
-import { NetworkCall } from "./networkcall";
-
-class AppAuth extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
-  }
-
-  componentDidMount(){
-    NetworkCall('meta.json','GET',null,null,false,true).then((_)=>{
-      localStorage.setItem('version',_.data.version)
-    }).catch(err=>{
-      console.log('err:', err);
-    })
-  }
-
-  render() {
-
-    let {
-      loading
-    } = this.props;
-
-    return (
-      <>       
-        {loading && (
-          <BorderLinearProgress/>
-        )}
-        {this.props.children}
-      </>
-    );
-  }
-}
-
-export default AppAuth;
+ import React from "react";
+ import { BorderLinearProgress } from "qdm-component-library";
+ import { NetworkCall } from "./networkcall";
+ import { initialMessageCatlog } from "atp-multilingual";
+ 
+ class AppAuth extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       intialLoad: true
+     };
+   }
+ 
+   async componentDidMount() {
+     NetworkCall('meta.json', 'GET', null, null, false, true).then((_) => {
+       localStorage.setItem('version', _.data.version)
+     }).catch(err => {
+       console.log('err:', err);
+     })
+     await initialMessageCatlog("METADATA_ID");
+     localStorage.setItem("language", "en")
+     this.setState({ intialLoad: false })
+   }
+ 
+   render() {
+ 
+     let { loading } = this.props;
+     let { intialLoad } = this.state;
+ 
+     return (
+       <>
+         {(loading || intialLoad) && (
+           <BorderLinearProgress />
+         )}
+         {!intialLoad && this.props.children}
+       </>
+     );
+   }
+ }
+ 
+ export default AppAuth;
+ 
